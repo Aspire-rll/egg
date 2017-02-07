70c88e35be0d3a6381f9e7d5d4bf2683f639634b
popomore <sakura9515@gmail.com>
2017-02-07 16:52:36 +0800

    f

---
+++ scripts/doc_travis.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/env bash
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+echo "key: $ENCRYPTED_KEY, iv: $ENCRYPTED_IV"
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+npm run doc-deploy
